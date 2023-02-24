48b0c7cf93294491c27bfcedb059d69f354c07dc
Maksym Dmyterko <max.dmytrenko@capgemini.com>
2023-02-24 20:48:33 +0100

    fix: overcoming the lazy loading issue

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,4 +1,16 @@
 import { createAction, createReducer, on } from '@ngrx/store';
+import { AppState } from 'src/app/state/app.state';
+import { Product } from '../product';
+
+export interface State extends AppState {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
 
 export const productReducer = createReducer(
   {
