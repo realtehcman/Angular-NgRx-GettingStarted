8e195c43cef3f5ce39bf6cfa252b21d18d56dd78
Maksym Dmyterko <max.dmytrenko@capgemini.com>
2023-02-24 17:21:23 +0100

    fix: convert imperative observables to declarative

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
 
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -8,31 +10,38 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   errorMessage: string;
 
-  displayCode: boolean;
+  displayCode$: Observable<Store> = this.store.select('products').pipe(
+    map((products: any) => {
+      if (products) {
+        return products.showProductCode;
+      }
+    })
+  );
 
-  products: Product[];
+  products$: Observable<Product[] | any> = this.productService.getProducts().pipe(
+    map((products: Product[]) => products),
+    catchError((err) => (this.errorMessage = err))
+  );
 
   // Used to highlight the selected product in the list
   selectedProduct: Product | null;
   sub: Subscription;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private store: Store<any>,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.sub = this.productService.selectedProductChanges$.subscribe(
-      selectedProduct => this.selectedProduct = selectedProduct
+      (selectedProduct) => (this.selectedProduct = selectedProduct)
     );
-
-    this.productService.getProducts().subscribe({
-      next: (products: Product[]) => this.products = products,
-      error: err => this.errorMessage = err
-    });
   }
 
   ngOnDestroy(): void {
@@ -40,7 +49,8 @@ export class ProductListComponent implements OnInit, OnDestroy {
   }
 
   checkChanged(): void {
-    this.displayCode = !this.displayCode;
+    this.store.dispatch({ type: '[Product] Toggle Product Code' });
+    // this.displayCode = !this.displayCode;
   }
 
   newProduct(): void {
@@ -50,5 +60,4 @@ export class ProductListComponent implements OnInit, OnDestroy {
   productSelected(product: Product): void {
     this.productService.changeSelectedProduct(product);
   }
-
 }
